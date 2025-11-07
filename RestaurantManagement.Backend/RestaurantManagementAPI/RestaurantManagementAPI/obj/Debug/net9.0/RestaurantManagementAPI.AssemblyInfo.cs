2946a28ec607a39570e0e8fa1db2806025111c38
todicode <nguyentrangiabao7100@gmail.com>
2025-11-07 23:55:33 +0700

    Latest feature code ready to push to dev

--- RestaurantManagement.Backend/RestaurantManagementAPI/RestaurantManagementAPI/obj/Debug/net9.0/RestaurantManagementAPI.AssemblyInfo.cs
+++ RestaurantManagement.Backend/RestaurantManagementAPI/RestaurantManagementAPI/obj/Debug/net9.0/RestaurantManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f50aa5abce6a802881d1767e1227c4c0eae6bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0f6ae79ca0ef96bfcd87c2a4903c97961b7394")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
